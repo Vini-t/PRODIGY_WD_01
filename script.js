059a14d19b98726cec8339c9d0941d5fec60d128
Vini-t <offficial.vini@gmail.com>
2025-07-09 11:12:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+// Vanta Background
+VANTA.WAVES({
+  el: "#vanta-bg",
+  mouseControls: true,
+  touchControls: true,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.0,
+  scaleMobile: 1.0,
+  color: 0xe15252,
+  shininess: 50.00,
+  waveHeight: 20.00,
+  waveSpeed: 0.8,
+  zoom: 1.1
+});
+
 // Sticky header effect
 window.addEventListener("scroll", () => {
   const navbar = document.getElementById("navbar");
